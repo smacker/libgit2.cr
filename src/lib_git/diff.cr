2706e2d9c924653f0e4bfb561a7286dbb318e22c
Håkan Nylén <confact@users.noreply.github.com>
2020-03-01 08:47:22 +0100

    Correct delta status enums

--- src/lib_git/diff.cr
+++ src/lib_git/diff.cr
@@ -27,8 +27,8 @@ lib LibGit
 
   enum DeltaT
     Unmodified =  0
-    Added      =  1
-    Deleted    =  2
+    Deleted    =  1
+    Added      =  2
     Modified   =  3
     Renamed    =  4
     Copied     =  5
